af43e4143a0326f13e9e8797e583b12127bc5017
airsyadth <rberpendidikan@gmail.com>
2025-07-17 07:53:25 +0700

    chore(route): apply role middleware to restrict access by role (Admin, Manajer Gudang, Staff Gudang)

--- resources/views/components/sidebar/managergudang-sidebar.blade.php
+++ resources/views/components/sidebar/managergudang-sidebar.blade.php
@@ -1,5 +1,5 @@
 <x-sidebar-dashboard>
-    <x-sidebar-menu-Managerdashboard routeName="index-practice" title="Dashboard"/>
+    <x-sidebar-menu-Managerdashboard routeName="manager-dashboard" title="Dashboard"/>
     <x-sidebar-menu-Managerproduk routeName="practice.seventh" title="Produk"/>
     <x-sidebar-menu-Managerstok routeName="practice.eighth" title="Stok"/>
     <x-sidebar-menu-Managersupplier routeName="practice.ninth" title="Supplier"/>
--- resources/views/components/sidebar/staffgudang-sidebar.blade.php
+++ resources/views/components/sidebar/staffgudang-sidebar.blade.php
@@ -1,5 +1,5 @@
 <x-sidebar-dashboard>
-    <x-sidebar-menu-Staffdashboard routeName="index-practice" title="Dashboard"/>
+    <x-sidebar-menu-Staffdashboard routeName="dashboard.staff" title="Dashboard"/>
     <x-sidebar-menu-staffstok routeName="practice.eleventh" title="Stok"/>
 </x-sidebar-dashboard>
 -
